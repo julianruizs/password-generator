21108940d3f93e4b934f204b3345ebd73e410658
Julian Ruiz <66812971+julianruizs@users.noreply.github.com>
2022-06-02 12:34:56 -0500

    Update settings

--- passwd_generator/settings.py
+++ passwd_generator/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-icboq$&81z!23uc@s2#!h_&7@&p$24ouz%f=nai*e8btm6uvdz'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 
 #DEBUG = False
